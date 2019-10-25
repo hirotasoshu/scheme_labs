214102686dd7da27a4ad216e6f63c11ac9627a68
hirotasoshu <rasetsu@list.ru>
2019-10-25 06:30:01 +0500

    added sieve of eratosthenes

---
+++ sieve_of_eratosthenes.rkt
@@ -0,0 +1,37 @@
+#lang scheme
+(define (list-with lst idx val)
+  (if (null? lst)
+      lst
+      (cons
+       (if (zero? idx)
+           val
+           (car lst))
+       (list-with (cdr lst) (- idx 1) val))))
+
+(define (true? el)
+  (eq? (cdr el) #t))
+
+(define (sieve n)
+  (define prime (build-list (+ n 1) (lambda (x) (cons x (< 1 2)))))
+  (define (while p prime)
+    (define (for i step prime)
+      (if (<= i n)
+          [for (+ i step) step (list-with prime i (cons i #f))]
+          [while (+ p 1) prime]))
+    (cond
+      [(> (sqr p) n) (list-tail prime 2)]
+      [(true? (list-ref prime p)) (for (* p p) p prime)]
+      [else (while (+ p 1) prime)]))
+  (while 2 prime))
+
+(define (primes-to n)
+  (map (lambda (el) (car el)) (filter true? (sieve n))))
+
+(primes-to 30)
+  
+       
+        
+        
+    
+ 
+          
