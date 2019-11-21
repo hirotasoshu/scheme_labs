4f19a9a7cde2147e6bd25ec3ac779b3814661d02
Maksim Zayakin <rasetsu@list.ru>
2019-11-22 03:11:19 +0500

    added task5 and remaked task4

--- scheme_lab_07/task_4.rkt
+++ scheme_lab_07/task_4.rkt
@@ -1,7 +1,12 @@
 #lang scheme
 (define (build-sublists lst)
-  (define (rec lst new-lst)
-    (if (empty? lst)
-        (reverse new-lst)
-        (rec (cdr lst) (cons lst new-lst))))
-  (rec lst '()))
+  (let* ([reverse_lst (reverse lst)] [car_lst (car reverse_lst)] [cdr_lst (cdr reverse_lst)])
+    (foldl (lambda (elem answer)
+             (if (= (length answer) 1)
+                 (cons (cons elem answer) (cons answer '()))
+                 (cons (cons elem (car answer)) answer))) 
+             (cons car_lst '()) cdr_lst)))
+
+           
+         
+  
---
+++ scheme_lab_07/task_5.rkt
@@ -0,0 +1,21 @@
+#lang scheme
+(require racket/format)
+
+(define (account-status sum percent protocol)
+  (define days (build-list 30 add1))
+  (define days-in-protocol (remove-duplicates (map (lambda (record)
+                                                     (car record)) protocol)))                          
+  (define percent_mult (+ 1 (/ percent 100)))
+  (define (get-sum-of-day sum day)
+    (define day-record (filter (lambda (record) (= (car record) day)) protocol))
+    (* (foldl (lambda (record sum)
+                ((eval (cadr record)) sum (caddr record))) sum day-record) percent_mult))
+  (foldl (lambda (day sum)
+         (if (member day days-in-protocol)
+             (get-sum-of-day sum day)
+             (* sum percent_mult))) sum days))
+
+(define (display-account-status sum percent protocol)
+  (display (~r (account-status sum percent protocol) #:precision 5)))
+             
+          
