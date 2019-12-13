74ae0d18366db19e65d14b86d841a4c988f0a175
Maksim Zayakin <rasetsu@list.ru>
2019-12-13 15:34:53 +0500

    fixed task_4.rkt

--- scheme_lab_10/task_4.rkt
+++ scheme_lab_10/task_4.rkt
@@ -7,8 +7,8 @@
   (define (iter-str str-lst substr substrs)
     (if (empty? str-lst)
         (if (empty-string? substr)
-        (reverse substrs)
-        (reverse (cons substr substrs)))
+            (reverse substrs)
+            (reverse (cons substr substrs)))
         (if (member (car str-lst) delim-lst)
             (if (empty-string? substr)
                 (iter-str (cdr str-lst) "" substrs)
@@ -20,12 +20,9 @@
   (define (iter-words words res)
     (if (empty? words)
         res
-        (if res
-            res
-                (if (ormap (lambda [word]
-                             (equal? word (car words))) (cdr words))
-                    (car words)
-                    (iter-words (cdr words) #f)))))
-  (iter-words words #f))
+        (if (ormap (lambda [word]
+                     (equal? word (car words))) (cdr words))
+            (car words)
+            (iter-words (cdr words) #f))))
+(iter-words words #f))
     
-  
